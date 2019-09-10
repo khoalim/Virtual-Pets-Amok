2e130f02ba8a3e263c699801d94f6d7991162250
Kim Hoalim <kthoalim@gmail.com>
2019-09-10 10:22:53 -0400

    Added Name and Species

--- VirtualPet/Program.cs
+++ VirtualPet/Program.cs
@@ -6,22 +6,65 @@ namespace VirtualPet
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Type PLAY to enter game or EXIT to leave");
-            string menu = Console.ReadLine().ToLower();
+            Console.WriteLine("Welcome to Virtual Pets Place!");          
 
-            if (menu == "play")
-            {
-                Console.WriteLine("Welcome to Virtual Pets Place!");
-                // Console.WriteLine("Would you like a real or robotic pet?");
-            }
-
-            else if (menu == "exit")
-            {
-                Console.WriteLine("Goodbye! Hit any key to exit");
-                Console.ReadKey();
-            }
-
+            Console.WriteLine("Play or Exit Game");                   
             
+            string playOptions = Console.ReadLine().ToLower();
+
+            if (playOptions == "play")
+            {
+                Console.WriteLine("Menu Options are:");
+                
+            }
+
+            else
+            {
+                Environment.Exit(0);
+
+            }
+
+            Console.WriteLine("1. Add Pet to Shelter");
+            Console.WriteLine("2. Exit Game");
+            string addPet = Console.ReadLine().ToLower();
+            
+
+            if (addPet == "add pet to shelter")
+            {
+                Console.WriteLine("What would you like to name your pet?");
+                string petName = Console.ReadLine();
+
+                Console.WriteLine("What species would you like?");
+                string petSpecies = Console.ReadLine();
+            }
+
+            else
+            {
+                Environment.Exit(0);
+            }
+
+
+
+
+
+            /*do
+            {
+                //if (playOptions == "play")
+                {
+                    //Console.WriteLine("play");
+                    
+                    //break;
+                }
+
+                //else if (playOptions == "exit")
+                {
+                    Console.WriteLine("Goodbye! Hit any key to exit");
+                    Console.ReadKey();
+                    //break;
+                }
+            }*/
+
+            // while (playOptions != "play" || playOptions != "exit");
 
 
         }
