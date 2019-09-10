a4029c4a3d32edbfa60995ed3c6c14af3d99828f
Kim Hoalim <kthoalim@gmail.com>
2019-09-10 11:57:20 -0400

    Refactoring  for Classes and Objects

--- VirtualPet/Program.cs
+++ VirtualPet/Program.cs
@@ -8,7 +8,7 @@ namespace VirtualPet
         {
             Console.WriteLine("Welcome to Virtual Pets Place!");          
 
-            Console.WriteLine("Play or Exit Game");                   
+            Console.WriteLine("Type Play or Exit Game");                   
             
             string playOptions = Console.ReadLine().ToLower();
 
@@ -27,11 +27,13 @@ namespace VirtualPet
             Console.WriteLine("1. Add Pet to Shelter");
             Console.WriteLine("2. Exit Game");
             string addPet = Console.ReadLine().ToLower();
-            
+            VirtualPet virtualPet = new VirtualPet();
 
             if (addPet == "add pet to shelter")
             {
                 Console.WriteLine("What would you like to name your pet?");
+                
+                virtualPet.Name = Console.ReadLine();
             }
 
             else
@@ -41,10 +43,12 @@ namespace VirtualPet
 
             
             string petName = Console.ReadLine();
-            int hungerLevel;
-            int boredomLevel;
-            int healthLevel;
-
+            int hungerLevel = 50;
+            int boredomLevel = 50;
+            int healthLevel = 50;
+            int feedPet = (hungerLevel - 20);
+            int boredomDecrease = (boredomLevel - 20); 
+            
             Console.WriteLine("What species would you like?");
             string petSpecies = Console.ReadLine();
 
@@ -58,39 +62,26 @@ namespace VirtualPet
 
             switch (petMenuChoice)
             {
-                case "view pet info": Console.WriteLine("Your pet's name is " + petName + " and its species is " + petSpecies);
+                case "view pet info": Console.WriteLine("Your pet's name is " + virtualPet.Name + " and its species is " + petSpecies);
                     break;
 
-                case "view pet status": Console.WriteLine("Your pet's hunger level is " + hungerLevel + "," + "its boredome level is " + boredomLevel + "," + "and its health level is " + healthLevel + ".");
+                case "view pet status": Console.WriteLine("Your pet's hunger level is " + hungerLevel + "," + " its boredome level is " + boredomLevel + "," + " and its health level is " + healthLevel + ".");
                     break;
-                case "feed pet": Console.WriteLine();
+
+                case "feed pet": Console.WriteLine(petName + "'s" + " hunger level decreased to " + feedPet + " after being fed."); 
                     break;
+
+                case "play with pet": Console.WriteLine();
+                    break;
+
+                case " take pet to doctor": Console.WriteLine();
+                    break;
+
             }
 
 
 
-
-
-            /*do
-            {
-                //if (playOptions == "play")
-                {
-                    //Console.WriteLine("play");
-                    
-                    //break;
-                }
-
-                //else if (playOptions == "exit")
-                {
-                    Console.WriteLine("Goodbye! Hit any key to exit");
-                    Console.ReadKey();
-                    //break;
-                }
-            }*/
-
-            // while (playOptions != "play" || playOptions != "exit");
-
-
+           
         }
     }
 }
